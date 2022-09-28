c04fef8b94cd7cf29db9da0901b046828d2a9f82
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-28 12:31:17 -0700

    initial file setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="UTF-8">
+    <title>Mock Title</title>
+</head>
+<body>
+  
+  <?php 
+
+  ?>
+
+
+</body>
+</html>

f78e219e6220383697b33e16e57897c0ed9226cd
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-09-30 14:44:42 -0700

    demonstrate adding data to db using prepared statements

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,19 +1,40 @@
 <?php
   include_once 'dbh.inc.php';
 
+  // THIS CODE: Will add a new user to the database
   //variables to save form data
   //mysqli_real_escape_string($conn, $_POST['first']); - this will stop code from being read in the database
+  // $first = mysqli_real_escape_string($conn, $_POST['first']);
+  // $last = mysqli_real_escape_string($conn, $_POST['last']);
+  // $email = mysqli_real_escape_string($conn, $_POST['email']);
+  // $uid = mysqli_real_escape_string($conn, $_POST['uid']);
+  // $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+  // // Creates new data into database
+
+  // $sql = "INSERT INTO users (first, last, email, uid, pwd) VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
+  // mysqli_query($conn, $sql);
+
+
+  // //loads home page after login with success in url
+  // header("location: ../index.php=success");
+
+
+  // THIS CODE: Will add users to the database using prepared statements, a safer way to add users to the database
   $first = mysqli_real_escape_string($conn, $_POST['first']);
   $last = mysqli_real_escape_string($conn, $_POST['last']);
   $email = mysqli_real_escape_string($conn, $_POST['email']);
   $uid = mysqli_real_escape_string($conn, $_POST['uid']);
   $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+  $sql = "INSERT INTO users (first, last, email, uid, pwd) VALUES (?, ?, ?, ?, ?);";
+  
+  $stmt = mysqli_stmt_init($conn);
+  if (!mysqli_stmt_prepare($stmt, $sql)){
+    echo "SQL ERROR";
+  } else {
+    // The second parameter uses s for string, and uses as many as needed (5 in this case)
+    mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $pwd, $uid);
+    mysqli_stmt_execute($stmt);
+  }
 
-  // Creates new data into database
-
-  $sql = "INSERT INTO users (first, last, email, uid, pwd) VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
-  mysqli_query($conn, $sql);
-
-
-  //loads home page after login with success in url
   header("location: ../index.php=success");
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  include_once 'includes/dbh.inc.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en-us">
 <head>
@@ -6,25 +10,58 @@
 </head>
 <body>
 
-    <!-- // Fetches all from users database and includes a loop to check if there is no result and also a condition/while loop for looping each result
+   <!-- Fetches all from users database and includes a loop to check if there is no result and also a condition/while loop for looping each result -->
 
-    // $sql = "SELECT * FROM users;";
-    // $result = mysqli_query($conn, $sql);
-    // $resultCheck = mysqli_num_rows($result);
-    // if ($resultCheck > 0) {
-    //   // loops while there is a result 
-    //   while ($row = mysqli_fetch_assoc($result)) {
-    //     echo $row['uid'] . "<br>";
-    //   }
-    // } -->
-
-  <form action="includes/signup.inc.php" method="POST">
+   <!-- $sql = "SELECT * FROM users;";
+   $result = mysqli_query($conn, $sql);
+   $resultCheck = mysqli_num_rows($result);
+   if ($resultCheck > 0) {
+     // loops while there is a result 
+     while ($row = mysqli_fetch_assoc($result)) {
+       echo $row['uid'] . "<br>";
+     }
+   } -->
+ 
+  <!-- <form action="includes/signup.inc.php" method="POST">
     <input type="text" name="first" placeholder="First Name"><br>
     <input type="text" name="last" placeholder="Last Name"><br>
     <input type="text" name="email" placeholder="E-mail"><br>
     <input type="text" name="uid" placeholder="User Name"><br>
     <input type="text" name="pwd" placeholder="Password"><br>
     <button type="submit" name="submit">Sign Up</button>
+    </form> -->
+
+
+    <?php
+      // using prepared statements is a safer way to search for data in a database
+      $data = "Admin";
+      // Created a template for grabbing a specific user
+      $sql = "SELECT * FROM users WHERE uid=?;";
+      // Create a prepated statement
+      $stmt = mysqli_stmt_init($conn);
+      // Prepare prepared statement
+      if(!mysqli_stmt_prepare($stmt, $sql)){
+        echo "SQL statement failed";
+      } else {
+        // Bind parameters to the placeholder
+        // The letter s in the parameter means the data type will be a string. s=string, i=integer, b=BLOB, d=double
+        mysqli_stmt_bind_param($stmt, "s", $data);
+        // Run parameters inside database
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        while ($row = mysqli_fetch_assoc($result)) {
+          echo $row['uid'] . "<br>";
+        }
+      }        
+    ?>
+
+    <form action="includes/signup.inc.php" method="POST">
+      <input type="text" name="first" placeholder="First Name"><br>
+      <input type="text" name="last" placeholder="Last Name"><br>
+      <input type="text" name="email" placeholder="E-mail"><br>
+      <input type="text" name="uid" placeholder="User Name"><br>
+      <input type="text" name="pwd" placeholder="Password"><br>
+      <button type="submit" name="submit">Sign Up</button>
     </form>
 </body>
 </html>

bb5613e5c5aab75c28bb97803548e3249a08e450
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-10-01 10:09:28 -0700

    demonstrate error handling with a form using php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Mock Title</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
@@ -56,12 +57,92 @@
     ?> -->
 
     <form action="includes/signup.inc.php" method="POST">
-      <input type="text" name="first" placeholder="First Name"><br>
-      <input type="text" name="last" placeholder="Last Name"><br>
-      <input type="text" name="email" placeholder="E-mail"><br>
-      <input type="text" name="uid" placeholder="User Name"><br>
+      <?php
+        if (isset($_GET['first'])) {
+          $first = $_GET['first'];
+          echo '<input type="text" name="first" placeholder="First Name" value="'.$first.'"><br>';
+        } else {
+          echo '<input type="text" name="first" placeholder="First Name"><br>';
+        }
+
+        if (isset($_GET['last'])) {
+          $last = $_GET['last'];
+          echo '<input type="text" name="last" placeholder="Last Name" value="'.$last.'"><br>';
+        } else {
+          echo '<input type="text" name="last" placeholder="Last Name"><br>';
+        }
+      ?>
+
+      <?php
+        if (isset($_GET['email'])) {
+          $email = $_GET['email'];
+          echo '<input type="text" name="email" placeholder="E-mail" value="'.$email.'"><br>';
+        } else {
+          echo '<input type="text" name="email" placeholder="E-mail"><br>';
+        }
+      ?>
+
+      <?php
+        if (isset($_GET['uid'])) {
+          $uid = $_GET['uid'];
+          echo '<input type="text" name="uid" placeholder="User Name" value="'.$uid.'"><br>';
+        } else {
+          echo '<input type="text" name="uid" placeholder="User Name"><br>';
+        }
+      ?>
+
       <input type="text" name="pwd" placeholder="Password"><br>
       <button type="submit" name="submit">Sign Up</button>
     </form>
+
+     <?php
+      // THIS CODE WILL: throw error messages if a certain string is in the url - gives error but also deletes info in form
+      // $fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+      // if (strpos($fullUrl, "signup=empty") == true) {
+      //   echo "<p class='error'>You did not fill in all the fields! Please fill in the missing fields.</p>";
+      //   exit();
+      // }
+      // elseif (strpos($fullUrl, "signup=char") == true) {
+      //   echo "<p class='error'>You used invalid characters in First and Last name</p>";
+      //   exit();
+      // }
+      // elseif (strpos($fullUrl, "signup=invalidemail") == true) {
+      //   echo "<p class='error'>You used an invalid e-mail</p>";
+      //   exit();
+      // }
+      // elseif (strpos($fullUrl, "signup=success") == true) {
+      //   echo "<p class='success'>You have successfully signed up!</p>";
+      //   exit();
+      // }
+
+      // THIS CODE WILL: Use GET method to throw an error from the form but also will not empty the fields
+        if (!isset($_GET['signup'])) {
+          exit();
+        }
+        else {
+          $signupCheck = $_GET['signup'];
+
+          if ($signupCheck == "empty") {
+            echo "<p class='error'>You did not fill in all the fields! Please fill in the missing fields.</p>";
+            exit();
+          }
+          elseif ($signupCheck == "char") {
+            echo "<p class='error'>You used invalid characters in First and Last name</p>";
+            exit();
+          }
+          elseif ($signupCheck == "invalidemail") {
+            echo "<p class='error'>You used an invalid e-mail</p>";
+            exit();
+          }
+          elseif ($signupCheck == "success") {
+            echo "<p class='success'>You have successfully signed up!</p>";
+            exit();
+          }
+        }
+    ?>
+
+    
+
 </body>
 </html>
